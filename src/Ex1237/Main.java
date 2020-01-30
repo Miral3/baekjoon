3ebe82e30a78d9fab24bdcc776c729fe7b157043
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-01-30 14:45:08 +0900

    add Ex1237 solution

---
+++ src/Ex1237/Main.java
@@ -0,0 +1,12 @@
+package Ex1237;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("문제의 정답");
+        br.close();
+    }
+}
