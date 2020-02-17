b8c271da8bc867b879cd6f3e12edcca198ecbe81
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-02-17 20:29:43 +0900

    add Ex1260 solution

---
+++ src/Ex1260/Main.java
@@ -0,0 +1,75 @@
+package Ex1260;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final int max = 10001;
+    static int node, edge, root, infoLeft, infoRight;
+    static boolean[] check;
+    static ArrayList<Integer> v[] = new ArrayList[max];
+    static Queue<Integer> bfsRoot = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        node = Integer.parseInt(st.nextToken());
+        edge = Integer.parseInt(st.nextToken());
+        root = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= node; i++){
+            v[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < edge; i++){
+            st = new StringTokenizer(br.readLine());
+            infoLeft = Integer.parseInt(st.nextToken());
+            infoRight = Integer.parseInt(st.nextToken());
+            v[infoLeft].add(infoRight);
+            v[infoRight].add(infoLeft);
+        }
+        for(int i = 1; i <= node; i++){
+            Collections.sort(v[i]);
+        }
+
+        check = new boolean[node+1];
+        dfs(root);
+        System.out.println();
+
+        check = new boolean[node+1];
+        bfs();
+        while(!bfsRoot.isEmpty()){
+            System.out.print(bfsRoot.poll()+" ");
+        }
+    }
+    public static void dfs(int x){
+        if(check[x])
+            return;
+        System.out.print(x+" ");
+        check[x] = true;
+        for(int i = 0; i < v[x].size(); i++){
+            if(!check[v[x].get(i)])
+                dfs(v[x].get(i));
+        }
+    }
+
+    public static void bfs(){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+        check[root] = true;
+
+        while(!q.isEmpty()){
+            int next = q.poll();
+            bfsRoot.add(next);
+            for(int i = 0; i < v[next].size(); i++){
+                if(!check[v[next].get(i)]) {
+                    check[v[next].get(i)] = true;
+                    q.add(v[next].get(i));
+                }
+            }
+        }
+    }
+}
