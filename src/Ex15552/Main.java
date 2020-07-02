3e07df7e0a1fa5fc8e272345dae4eead7deefee4
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-07-02 21:12:42 +0900

    add Ex15552 solution

---
+++ src/Ex15552/Main.java
@@ -0,0 +1,23 @@
+package Ex15552;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A+B + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
