8b373c6d94b3fa54ae32652da50fbc980a0efc71
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-07-02 21:12:25 +0900

    add Ex10950 solution

---
+++ src/Ex10950/Main.java
@@ -0,0 +1,20 @@
+package Ex10950;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A+B);
+        }
+        br.close();
+    }
+}
