e137eae9778007a93f4ea54a575bc713bf7c11d6
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-07-02 21:12:34 +0900

    add Ex11022 solution

---
+++ src/Ex11022/Main.java
@@ -0,0 +1,22 @@
+package Ex11022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= input; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
+        }
+        br.close();
+    }
+}
