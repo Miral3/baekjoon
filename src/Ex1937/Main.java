85e62670e51ae2abeb81d237a40041a4260c87cb
YongSangYou <dydtkd113@gamil.com>
2020-07-30 14:30:57 +0900

    try Ex1937 problem

--- src/Ex1937/Main.java
+++ src/Ex1937/Main.java
@@ -1,57 +1,61 @@
-package Ex1937;
-
-import java.io.*;
-
-public class Main {
-    public static int max = 1, n, arr[][], dp[][], dx[]={0, 0, -1, 1}, dy[]={1, -1, 0, 0};
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int i, j; n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        dp = new int[n][n];
-        String line[];
-
-        for(i = 0; i < n; i++){
-            line = br.readLine().split(" ");
-            for(j = 0; j < n; j++){
-                arr[i][j] = Integer.parseInt(line[j]);
-            }
-        }
-
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n; j++){
-                if(dp[i][j] == 0) {
-                    dp[i][j] = 1;
-                    mov(i, j);
-                }
-            }
-        }
-        bw.write(String.valueOf(max));
-        bw.close();
-        br.close();
-    }
-    private static void mov(int x, int y){
-        int ax, ay, i;
-        for(i = 0; i < 4; i++){
-            ax = x + dx[i];
-            ay = y + dy[i];
-
-            if(isInRange(ax, ay) && shouldMov(x, y, ax, ay)){
-                dp[ax][ay] = dp[x][y] + 1;
-                if(max < dp[ax][ay])
-                    max = dp[ax][ay];
-                mov(ax,ay);
-            }
-        }
-    }
-
-    private static boolean shouldMov(int x, int y, int ax, int ay){
-        return (arr[x][y] < arr[ax][ay] && dp[ax][ay] < dp[x][y] + 1);
-    }
-
-    private static boolean isInRange(int x, int y){
-        return (0 <= x && x < n) && (0 <= y && y < n);
-    }
-}
+//package Ex1937;
+//
+//import java.io.BufferedWriter;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//
+//public class Main {
+//    public static int max = 1, n, arr[][], dp[][], dx[]={0, 0, -1, 1}, dy[]={1, -1, 0, 0};
+//
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int i, j; n = Integer.parseInt(br.readLine());
+//        arr = new int[n][n];
+//        dp = new int[n][n];
+//        String line[];
+//
+//        for(i = 0; i < n; i++){
+//            line = br.readLine().split(" ");
+//            for(j = 0; j < n; j++){
+//                arr[i][j] = Integer.parseInt(line[j]);
+//            }
+//        }
+//
+//        for(i = 0; i < n; i++){
+//            for(j = 0; j < n; j++){
+//                if(dp[i][j] == 0) {
+//                    dp[i][j] = 1;
+//                    mov(i, j);
+//                }
+//            }
+//        }
+//        bw.write(String.valueOf(max));
+//        bw.close();
+//        br.close();
+//    }
+//    private static void mov(int x, int y){
+//        int ax, ay, i;
+//        for(i = 0; i < 4; i++){
+//            ax = x + dx[i];
+//            ay = y + dy[i];
+//
+//            if(isInRange(ax, ay) && shouldMov(x, y, ax, ay)){
+//                dp[ax][ay] = dp[x][y] + 1;
+//                if(max < dp[ax][ay])
+//                    max = dp[ax][ay];
+//                mov(ax,ay);
+//            }
+//        }
+//    }
+//
+//    private static boolean shouldMov(int x, int y, int ax, int ay){
+//        return (arr[x][y] < arr[ax][ay] && dp[ax][ay] < dp[x][y] + 1);
+//    }
+//
+//    private static boolean isInRange(int x, int y){
+//        return (0 <= x && x < n) && (0 <= y && y < n);
+//    }
+//}
