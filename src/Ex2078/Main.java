7b5b654a197fe4d2d56bfc4bc773cf66b6a81c96
YongSangYou <dydtkd113@gamil.com>
2020-09-16 11:24:14 +0900

    add Ex2078 solution

---
+++ src/Ex2078/Main.java
@@ -0,0 +1,30 @@
+package Ex2078;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int left = 0;
+        int right = 0;
+        while(true) {
+            if(A > B) {
+                A -= B;
+                left++;
+            }
+            else {
+                B -= A;
+                right++;
+            }
+            if(A == 1 && B == 1) {
+                break;
+            }
+        }
+        System.out.println(left + " " + right);
+    }
+}
