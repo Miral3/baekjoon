65be3ecacd7e5c8a9aee469fb9bb3f194c0a105a
YongSangYou <dydtkd113@gamil.com>
2020-09-16 10:00:47 +0900

    add Ex1544 refactoring

--- src/Ex1544/Main.java
+++ src/Ex1544/Main.java
@@ -3,14 +3,14 @@ package Ex1544;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
         String[] cycle = new String[testCase];
-        LinkedList<String> list = new LinkedList<>();
+        HashSet<String> set = new HashSet<>();
 
         for(int i = 0; i < testCase; i++) {
             cycle[i] = br.readLine();
@@ -18,15 +18,15 @@ public class Main {
 
         int cnt = 0;
         for(int j = 0; j < cycle.length; j++) {
-            if(!list.contains(cycle[j])) {
+            if(!set.contains(cycle[j])) {
                 cnt++;
             }
             for (int i = 1; i <= cycle[j].length(); i++) {
                 String split = cycle[j].substring(i, cycle[j].length()) + cycle[j].substring(0, i);
-                if(list.contains(split)) {
+                if(set.contains(split)) {
                     break;
                 }
-                list.add(split);
+                set.add(split);
             }
         }
         System.out.println(cnt);
