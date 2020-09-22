56bdd282dbd1793104e3738a12f8b3e8fbf568bb
YongSangYou <dydtkd113@gamil.com>
2020-09-22 16:21:01 +0900

    add Ex11650 solution

---
+++ src/Ex11650/Main.java
@@ -0,0 +1,31 @@
+package Ex11650;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        int[][] arr = new int[testCase][2];
+
+        for(int i = 0; i < testCase; i++) {
+            String[] input = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(input[0]);
+            arr[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            }else {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+        for(int i = 0 ; i < testCase; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
