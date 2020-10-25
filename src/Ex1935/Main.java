92990ee5b331b8fb9f9ec6b55a10dfe1bd8d2494
YongSangYou <dydtkd113@gamil.com>
2020-10-25 17:38:05 +0900

    add Ex1935 solution

--- src/Ex1935/Main.java
+++ src/Ex1935/Main.java
@@ -7,46 +7,46 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(br.readLine()); //피연산자의 갯수를 입력
-        char[] input = br.readLine().toCharArray(); //후위표기식 입력
-        Stack<Double> stack = new Stack<>(); //계산할 스택
-        double[] numArr = new double[26]; //피연산자의 값을 저장할 배열
-        int j = 0;
+        int testCase = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        Stack<Double> stack = new Stack<>();
+        int[] insert = new int[testCase];
 
-        for (int i = 0; i < num; i++) {
-            double val = Double.parseDouble(br.readLine());
-            numArr[i] = val;
+        for(int i = 0; i < testCase; i++) {
+            insert[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = 0; i < input.length; i++) {
-            double oper; //연산을 만나는 첫번째 피연산
-            double result; //연산이랑 연속되는 2개의 피연산자들의 계산 값
-            switch (input[i]) {
-                case '+':
-                    oper = stack.pop();
-                    result = (stack.pop() + oper);
-                    stack.push(result);
+        for(int i = 0; i < input.length(); i++) {
+            char idx = input.charAt(i);
+            double x = 0;
+            double y = 0;
+            double result;
+            switch(idx) {
+                case '+':   y = stack.pop();
+                            x = stack.pop();
+                            result = x+y;
+                            stack.add(result);
+                            break;
+                case '-':   y = stack.pop();
+                    x = stack.pop();
+                    result = x-y;
+                    stack.add(result);
                     break;
-                case '-':
-                    oper = stack.pop();
-                    result = (stack.pop() - oper);
-                    stack.push(result);
+                case '*':   y = stack.pop();
+                    x = stack.pop();
+                    result = x*y;
+                    stack.add(result);
                     break;
-                case '*':
-                    oper = stack.pop();
-                    result = (stack.pop() * oper);
-                    stack.push(result);
+                case '/':   y = stack.pop();
+                    x = stack.pop();
+                    result = x/y;
+                    stack.add(result);
                     break;
-                case '/':
-                    oper = stack.pop();
-                    result = (stack.pop() / oper);
-                    stack.push(result);
+                    default: result = insert[(int)idx-65];
+                    stack.add(result);
                     break;
-                default:
-                    stack.push(numArr[(int)input[i] - 65]);
             }
         }
-        System.out.println(String.format("%.2f",stack.pop()));
-        br.close();
+        System.out.println(String.format("%.2f", stack.peek()));
     }
 }
