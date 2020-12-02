2beac878cf1fdfec38d36d060231efe24101dfe8
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-12-02 09:22:11 +0900

    add Ex10988 solution

---
+++ src/Ex10988/Main.java
@@ -0,0 +1,21 @@
+package Ex10988;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        char[] chop = input.toCharArray();
+
+        for(int i = 0; i < chop.length/2; i++) {
+            if(chop[i] != chop[chop.length-1-i]) {
+                System.out.println("0");
+                return;
+            }
+        }
+        System.out.println("1");
+    }
+}
