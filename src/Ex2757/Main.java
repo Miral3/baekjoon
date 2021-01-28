bdf4be735b34fc0985b3fdced7ba03ffb3396c92
YongSangYou <dydtkd113@gamil.com>
2021-01-28 14:43:35 +0900

    edit Ex2757 solution

--- src/Ex2757/Main.java
+++ src/Ex2757/Main.java
@@ -12,7 +12,7 @@ public class Main {
                 StringBuffer strBuffer = new StringBuffer();
                 int A = Integer.parseInt(input[1]);
                 int B = Integer.parseInt(input[2]);
-                String answer = "";
+//                String answer = "";
                 if(A==0 && B == 0) {
                     break;
                 }
@@ -21,16 +21,18 @@ public class Main {
                     B /= 26;
 
                     if (remain == 0) {
-                        answer += 'Z';
+//                        answer += 'Z';
+                        strBuffer.append('Z');
                     } else {
-                        answer += (char) (remain + 64);
+//                        answer += (char) (remain + 64);
+                        strBuffer.append((char) (remain + 64));
                     }
                     if (remain == 0) {
                         B--;
                     }
 
                 }
-                strBuffer.append(answer);
+//                strBuffer.append(answer);
                 bw.write(strBuffer.reverse().toString());
                 bw.write(A+"\n");
             }
