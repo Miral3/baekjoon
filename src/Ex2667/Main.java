5a3f963dbcd38b09f36198343551a26ae6f3c4f6
YongSangYou <dydtkd113@gamil.com>
2021-01-30 17:46:14 +0900

    add Ex2667 solution

--- src/Ex2667/Main.java
+++ src/Ex2667/Main.java
@@ -6,64 +6,54 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
-    static int count;
-    static char[][] map;
+    static int cnt = 0, N;
+    static boolean[][] visited;
+    static String[][] map;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 1, 0, 0};
     static PriorityQueue<Integer> q = new PriorityQueue<>();
-    static boolean[][] isVisited;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size;
-        size = Integer.parseInt(br.readLine());
-        map = new char[size][size];
-        isVisited = new boolean[size][size];
 
-        for(int i = 0; i < size; i ++){
-            String dwell = br.readLine();
-            for(int j = 0; j < size; j++){
-                map[i][j]=(dwell.charAt(j));
+        N = Integer.parseInt(br.readLine());
+        String[] input;
+        map = new String[N][N];
+        visited = new boolean[N][N];
+
+        for(int i = 0; i < N; i++) {
+            input = br.readLine().split("");
+            for(int j = 0; j < N; j++) {
+                map[i][j] = input[j];
             }
         }
 
-
-        for(int i = 0; i < size; i ++){
-            for(int j = 0; j < size; j++){
-                if((!isVisited[i][j]) && map[i][j] == '1') {
-                    isVisited[i][j] = true;
-                    count = 1;
-                    dfs(i, j, size);
-                    q.add(count);
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(map[i][j].equals("1") && !visited[i][j]) {
+                    cnt = 1;
+                    DFS(i, j);
+                    q.add(cnt);
                 }
             }
         }
-
-        int qSize = q.size();
-        System.out.println(qSize);
-        for(int i = 0; i < qSize; i++){
+        int size = q.size();
+        System.out.println(size);
+        for(int i = 0; i < size; i++) {
             System.out.println(q.poll());
         }
     }
-    public static void dfs(int x, int y, int size){
-        if(!(x-1 < 0) && map[x-1][y] == '1' && (!isVisited[x-1][y])){
-            count++;
-            isVisited[x-1][y] = true;
-            dfs(x-1, y, size);
-        }
-        if(!(x+1 >= size) && map[x+1][y] == '1' && (!isVisited[x+1][y])){
-            count++;
-            isVisited[x+1][y] = true;
-            dfs(x+1, y, size);
-        }
-        if(!(y-1 < 0) && map[x][y-1] == '1' && (!isVisited[x][y-1])){
-            count++;
-            isVisited[x][y-1] = true;
-            dfs(x, y-1, size);
-        }
-        if(!(y+1 >= size) && map[x][y+1] == '1' && (!isVisited[x][y+1])){
-            count++;
-            isVisited[x][y+1] = true;
-            dfs(x, y+1, size);
-        }
+    public static void DFS(int x, int y) {
+        visited[x][y] = true;
 
+        for(int i =0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if(map[nx][ny].equals("1") && !visited[nx][ny]) {
+                    cnt++;
+                    DFS(nx,ny);
+                }
+            }
+        }
     }
 }
