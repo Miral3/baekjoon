64079fd2a3db28c6cb83b378281259afbbba42da
YongSangYou <dydtkd113@gamil.com>
2021-01-30 16:06:30 +0900

    edit Ex2606 solution

--- src/Ex2606/Main.java
+++ src/Ex2606/Main.java
@@ -1,57 +1,54 @@
 package Ex2606;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
+    static final int max = 10001;
+    static int node, edge, root = 1, infoLeft, infoRight, cnt=0;
+    static boolean[] check;
+    static ArrayList<Integer> v[] = new ArrayList[max];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputA;
-        int inputB;
-        String[] input;
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N+1; i++) {
-            graph.add(new ArrayList<>());
+        node = Integer.parseInt(br.readLine());
+        edge = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= node; i++){
+            v[i] = new ArrayList<>();
         }
 
-        for(int i = 0; i < M; i++) {
-            input = br.readLine().split(" ");
-            inputA = Integer.parseInt(input[0]);
-            inputB = Integer.parseInt(input[1]);
-            graph.get(inputA).add(inputB);
-            graph.get(inputB).add(inputA);
+        for(int i = 0; i < edge; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            infoLeft = Integer.parseInt(st.nextToken());
+            infoRight = Integer.parseInt(st.nextToken());
+            v[infoLeft].add(infoRight);
+            v[infoRight].add(infoLeft);
+        }
+        for(int i = 1; i <= node; i++){
+            Collections.sort(v[i]);
         }
 
-        BFS(graph);
-
-        br.close();
+        check = new boolean[node+1];
+        bfs();
     }
 
-    public static void BFS(ArrayList<ArrayList<Integer>> graph) {
-        LinkedList<Integer> que = new LinkedList<>();
-        boolean[] check = new boolean[N+1];
-        int cnt = 0;
+    public static void bfs(){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+        check[root] = true;
 
-        que.add(1);
-        check[1] = true;
-
-        while(!que.isEmpty()) {
-            int rm = que.remove();
-            for(int nxtRm : graph.get(rm)) {
-                if(!check[nxtRm]) {
-                    check[nxtRm] = true;
-                    que.add(nxtRm);
+        while(!q.isEmpty()){
+            int next = q.poll();
+            for(int i = 0; i < v[next].size(); i++){
+                if(!check[v[next].get(i)]) {
+                    check[v[next].get(i)] = true;
+                    q.add(v[next].get(i));
                     cnt++;
                 }
             }
         }
-
         System.out.println(cnt);
     }
 }
