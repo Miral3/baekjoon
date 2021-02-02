fae0e342521a98fbdabba0050f2b73ac7e905fce
YongSangYou <dydtkd113@gamil.com>
2021-02-02 15:40:07 +0900

    add Ex7576 solution

--- src/Ex7576/Main.java
+++ src/Ex7576/Main.java
@@ -1,16 +1,20 @@
 package Ex7576;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static boolean[][] check;
+    static boolean[][] visited;
+    static int row, column, cnt = 0;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int[][] box;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int row;
-        int column;
-        int[][] box;
         String[] input;
 
         input = br.readLine().split(" ");
@@ -18,7 +22,7 @@ public class Main {
         column = Integer.parseInt(input[1]);
 
         box = new int[column][row];
-        check = new boolean[column][row];
+        visited = new boolean[column][row];
         for(int i = 0; i < column; i++) {
             input = br.readLine().split(" ");
             for(int j = 0; j < row; j++) {
@@ -26,10 +30,49 @@ public class Main {
             }
         }
 
+        BFS();
+        System.out.println(cnt);
+    }
+
+    public static void BFS() {
+        Queue<Point> q = new LinkedList<>();
+        
         for(int i = 0; i < column; i++) {
             for(int j = 0; j < row; j++) {
-                if(!check[i][j] && box[i][j] == 1) {
+                if(box[i][j] == 1) {
+                    q.add(new Point(i, j));
+                    visited[i][j] = true;
+                }
+            }
+        }
 
+        int size = q.size();
+
+        while(!q.isEmpty()) {
+            int x = q.peek().x;
+            int y = q.peek().y;
+            if(size == 0) {
+                size = q.size();
+                cnt++;
+            }
+            q.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && ny >= 0 && nx < column && ny < row) {
+                    if(box[nx][ny] == 0 && !visited[nx][ny]) {
+                        q.add(new Point(nx,ny));
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+            size--;
+        }
+
+        for(int i = 0; i < column; i++) {
+            for(int j = 0; j < row; j++) {
+                if(box[i][j] == 0 && !visited[i][j]) {
+                    cnt = -1;
                 }
             }
         }
