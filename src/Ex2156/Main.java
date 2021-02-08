772db71deb03b9c97a1d163f6a09bf4568b1fcc1
YongSangYou <dydtkd113@gamil.com>
2021-02-08 15:14:57 +0900

    add Ex2156 solution

--- src/Ex2156/Main.java
+++ src/Ex2156/Main.java
@@ -1,31 +1,42 @@
 package Ex2156;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        int[] dp = new int[testCase+1];
-        int[] wine = new int[testCase+1];
-        int caseOne, caseTwo, caseThr;
+    static int[] wine;
+    static int[] wineData;
 
-        for(int i = 1; i <= testCase; i++){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCase = Integer.parseInt(br.readLine());
+        wine = new int[10001];
+        wineData = new int[10001];
+
+        for(int i = 1; i <= testCase; i++) {
             int input = Integer.parseInt(br.readLine());
             wine[i] = input;
         }
+        bw.write(dp(testCase)+"");
 
-        dp[1] = wine[1];
-        dp[2] = wine[1] + wine[2];
-        for(int i = 3; i <= testCase; i++){
-            caseOne = dp[i-1];
-            caseTwo = dp[i-2] + wine[i];
-            caseThr = wine[i-1] + wine[i];
-            dp[i] = Math.max(caseOne, Math.max(caseTwo, caseThr));
+        br.close();
+        bw.close();
+    }
+    public static int dp(int n) {
+        wineData[1] = wine[1];
+        wineData[2] = wine[1] + wine[2];
+
+        for(int i = 3; i <= n; i++) {
+            int once = wineData[i-2] + wine[i];
+            int twice = wineData[i-3] + wine[i] + wine[i-1];
+            int thrice = wineData[i-1];
+            wineData[i] = Math.max(thrice,Math.max(once, twice));
         }
-
-        System.out.println(dp[testCase]);
+        return wineData[n];
     }
 }
