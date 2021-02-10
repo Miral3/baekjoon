0ff43681ecdd03e393a34a497ac5c20cefda739f
YongSangYou <dydtkd113@gamil.com>
2021-02-10 18:17:26 +0900

    add Ex9461 solution

--- src/Ex9461/Main.java
+++ src/Ex9461/Main.java
@@ -1,27 +1,39 @@
 package Ex9461;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int testCase = Integer.parseInt(br.readLine());
-        long arr[] = new long[101];
+        long[] dp = new long[101];
+        int[] N = new int[testCase];
 
-        arr[0] = 1;
-        arr[1] = 1;
-        arr[2] = 1;
-
-        for(int i = 3; i < arr.length; i++){
-            arr[i] = arr[i-3] + arr[i-2];
-        }
-
-        for(int i = 0; i < testCase; i++){
+        for(int i = 0; i < testCase; i++) {
             int input = Integer.parseInt(br.readLine());
-            System.out.println(arr[input-1]);
+            N[i] = input;
         }
+
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for(int i = 4; i < 101; i++) {
+            dp[i] = dp[i-3] + dp[i-2];
+        }
+
+        for(int i = 0; i < testCase; i++) {
+            bw.write(dp[N[i]] + "\n");
+        }
+
+        br.close();
+        bw.close();
     }
 }
