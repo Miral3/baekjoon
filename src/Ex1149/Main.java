9a229b1c605993ee5ad1a4d0ae2fc2e29f4104aa
YongSangYou <dydtkd113@gamil.com>
2021-02-11 14:05:39 +0900

    add Ex1149 solution

--- src/Ex1149/Main.java
+++ src/Ex1149/Main.java
@@ -9,19 +9,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int testCase = Integer.parseInt(br.readLine());
-        int[][] rgbPrice = new int[testCase+1][4];
-        String[] price = br.readLine().split(" ");
+        int[][] paintCost = new int[testCase][3];
+        int[][] dp = new int[testCase][3];
 
-        rgbPrice[1][1] = Integer.parseInt(price[0]);
-        rgbPrice[1][2] = Integer.parseInt(price[1]);
-        rgbPrice[1][3] = Integer.parseInt(price[2]);
+        for(int i = 0; i < testCase; i++) {
+            String[] input = br.readLine().split(" ");
+            int R = Integer.parseInt(input[0]);
+            int G = Integer.parseInt(input[1]);
+            int B = Integer.parseInt(input[2]);
 
-        for(int i = 2; i < testCase+1; i++){
-            price = br.readLine().split(" ");
-            rgbPrice[i][1] = Math.min(rgbPrice[i-1][2], rgbPrice[i-1][3])+Integer.parseInt(price[0]);
-            rgbPrice[i][2] = Math.min(rgbPrice[i-1][1], rgbPrice[i-1][3])+Integer.parseInt(price[1]);
-            rgbPrice[i][3] = Math.min(rgbPrice[i-1][1], rgbPrice[i-1][2])+Integer.parseInt(price[2]);
+            paintCost[i][0] = R;
+            paintCost[i][1] = G;
+            paintCost[i][2] = B;
         }
-        System.out.println(Math.min(rgbPrice[testCase][1],Math.min(rgbPrice[testCase][2], rgbPrice[testCase][3])));
+
+        dp[0][0] = paintCost[0][0];
+        dp[0][1] = paintCost[0][1];
+        dp[0][2] = paintCost[0][2];
+
+        for(int i = 1; i < testCase; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + paintCost[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + paintCost[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + paintCost[i][2];
+        }
+        System.out.println(Math.min(dp[testCase-1][0],
+            Math.min(dp[testCase-1][1], dp[testCase-1][2])));
     }
 }
