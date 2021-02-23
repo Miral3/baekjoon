a43b6f85f516d57ca24e44a544c4aca32ee5ac81
YongSangYou <dydtkd113@gamil.com>
2021-02-23 16:36:17 +0900

    add Ex11727 solution

---
+++ src/Ex11727/Main.java
@@ -0,0 +1,29 @@
+package Ex11727;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public  static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    int[] dp = new int[10001];
+
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for(int i = 3; i <= N; i++) {
+      dp[i] = ((2*dp[i-2]) + dp[i-1])%10007;
+    }
+
+    bw.write(dp[N] + "");
+
+    br.close();
+    bw.close();
+  }
+}
