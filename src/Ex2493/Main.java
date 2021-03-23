1313f389748f188aa375d31cff062a4fa88e1a2a
YongSangYou <dydtkd113@gmail.com>
2021-03-23 13:52:11 +0900

    add Ex2493 solution

--- src/Ex2493/Main.java
+++ src/Ex2493/Main.java
@@ -1,34 +1,50 @@
-//package Ex2493;
-//
-//import kotlin.Pair;
-//
-//import java.io.IOException;
-//import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.util.StringTokenizer;
-//import java.util.Stack;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int testCase = Integer.parseInt(br.readLine());
-//        Stack <Pair<Long, Long>> top = new Stack<>();
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//
-//        for(long i = 0; i < testCase; i++){
-//            long input = Long.parseLong(st.nextToken());
-//            while(!top.isEmpty()){
-//                if(top.peek().getSecond() > input){
-//                    System.out.print(top.peek().getFirst()+" ");
-//                    break;
-//                }
-//                top.pop();
-//            }
-//            if(top.isEmpty())
-//                System.out.print("0 ");
-//
-//            top.push(new Pair<>(i+1, input));
-//        }
-//    }
-//}
-//
+package Ex2493;
+
+/*
+문제 이름 : 탑
+알고리즘 : 스택
+자료구조 : 스택
+ */
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    Stack<Integer> top = new Stack<>();
+    Stack<Integer> index = new Stack<>();
+
+    for(int i = 1; i <= N; i++) {
+      int num = Integer.parseInt(st.nextToken());
+
+      while(!top.isEmpty()) {
+        if(num <= top.peek()) {
+          bw.write(index.peek() + " ");
+          break;
+        }
+        top.pop();
+        index.pop();
+      }
+      if(top.empty()) {
+        bw.write("0 ");
+      }
+      top.add(num);
+      index.add(i);
+    }
+
+    br.close();
+    bw.close();
+  }
+}
+
