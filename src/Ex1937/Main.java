6e55a82edd8ada0d8dc8e6208d4201f1c8802ce0
YongSangYou <dydtkd113@gmail.com>
2021-05-25 18:14:05 +0900

    add Ex1937 solution

--- src/Ex1937/Main.java
+++ src/Ex1937/Main.java
@@ -1,61 +1,66 @@
-//package Ex1937;
-//
-//import java.io.BufferedWriter;
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
-//
-//public class Main {
-//    public static int max = 1, n, arr[][], dp[][], dx[]={0, 0, -1, 1}, dy[]={1, -1, 0, 0};
-//
-//    public static void main(String[] args) throws IOException{
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        int i, j; n = Integer.parseInt(br.readLine());
-//        arr = new int[n][n];
-//        dp = new int[n][n];
-//        String line[];
-//
-//        for(i = 0; i < n; i++){
-//            line = br.readLine().split(" ");
-//            for(j = 0; j < n; j++){
-//                arr[i][j] = Integer.parseInt(line[j]);
-//            }
-//        }
-//
-//        for(i = 0; i < n; i++){
-//            for(j = 0; j < n; j++){
-//                if(dp[i][j] == 0) {
-//                    dp[i][j] = 1;
-//                    mov(i, j);
-//                }
-//            }
-//        }
-//        bw.write(String.valueOf(max));
-//        bw.close();
-//        br.close();
-//    }
-//    private static void mov(int x, int y){
-//        int ax, ay, i;
-//        for(i = 0; i < 4; i++){
-//            ax = x + dx[i];
-//            ay = y + dy[i];
-//
-//            if(isInRange(ax, ay) && shouldMov(x, y, ax, ay)){
-//                dp[ax][ay] = dp[x][y] + 1;
-//                if(max < dp[ax][ay])
-//                    max = dp[ax][ay];
-//                mov(ax,ay);
-//            }
-//        }
-//    }
-//
-//    private static boolean shouldMov(int x, int y, int ax, int ay){
-//        return (arr[x][y] < arr[ax][ay] && dp[ax][ay] < dp[x][y] + 1);
-//    }
-//
-//    private static boolean isInRange(int x, int y){
-//        return (0 <= x && x < n) && (0 <= y && y < n);
-//    }
-//}
+package Ex1937;
+
+/*
+문제 이름 : 욕심쟁이 판다
+알고리즘 : 다이나믹 프로그래밍
+자료구조 : 배열
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  static int N;
+  static int[][] map;
+  static int[][] dp;
+  static int[] dx = {1, -1, 0, 0};
+  static int[] dy = {0 ,0 , 1, -1};
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    N = Integer.parseInt(br.readLine());
+    map = new int[N][N];
+    dp = new int[N][N];
+
+    for(int i = 0; i < N; i++) {
+      String[] input = br.readLine().split(" ");
+      for(int j = 0; j < N; j++) {
+        map[i][j] = Integer.parseInt(input[j]);
+      }
+    }
+    int result = 0;
+    for(int i = 0; i < N; i++) {
+      for(int j = 0; j < N; j++) {
+        result = Math.max(DFS(i, j), result );
+      }
+    }
+    System.out.println(result);
+    br.close();
+    bw.close();
+  }
+
+  public static int DFS(int y, int x) {
+    if(dp[y][x] == 0) {
+      dp[y][x] = 1;
+      int temp = 0;
+      for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+          if (map[y][x] < map[ny][nx]) {
+            temp = Math.max(temp, DFS(ny, nx));
+          }
+        }
+      }
+      dp[y][x] += temp;
+    }
+    return dp[y][x];
+  }
+}
+
