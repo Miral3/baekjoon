46ad38bebeb42f9871a7b06551a45d7856add535
YongSangYou <dydtkd113@gmail.com>
2021-06-01 17:05:24 +0900

    add Ex1753 solution

--- src/Ex1753/Main.java
+++ src/Ex1753/Main.java
@@ -12,31 +12,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Main {
-  static final int MAX = 20001;
+  static int V, E, K;
   static int[] d;
-  static ArrayList<Node>  vector[] = new ArrayList[MAX];
+  static List<Node>[] list;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     String[] input = br.readLine().split(" ");
-    int V = Integer.parseInt(input[0]);
-    int E = Integer.parseInt(input[1]);
-    int K = Integer.parseInt(br.readLine());
+    V = Integer.parseInt(input[0]);
+    E = Integer.parseInt(input[1]);
+    K = Integer.parseInt(br.readLine());
+
+    list = new ArrayList[V + 1];
+    d = new int[V + 1];
 
     for(int i = 1; i <= V; i++) {
-      vector[i] = new ArrayList<>();
+      list[i] = new ArrayList<>();
     }
 
-    d = new int[V+1];
-    for(int i = 1; i <= V; i++) {
-      d[i] = Integer.MAX_VALUE;
-    }
+    Arrays.fill(d, Integer.MAX_VALUE);
 
     for(int i = 0; i < E; i++) {
       input = br.readLine().split(" ");
@@ -44,7 +45,7 @@ public class Main {
       int v = Integer.parseInt(input[1]);
       int w = Integer.parseInt(input[2]);
 
-      vector[u].add(new Node(v, w));
+      list[u].add(new Node(v, w));
     }
 
     dijkstra(K);
@@ -62,56 +63,39 @@ public class Main {
   }
 
   public static void dijkstra(int start) {
+    PriorityQueue<Node> pq = new PriorityQueue();
+    boolean[] check = new boolean[V + 1];
+    pq.add(new Node(start, 0));
     d[start] = 0;
 
-    PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>() {
-      @Override
-      public int compare(Node o1, Node o2) {
-        if(o1.y < o2.y) {
-          return -1;
-        } else if(o1.y == o2.y) {
-          return 0;
-        } else {
-          return 1;
-        }
-      }
-    });
-
-    pq.add(new Node(start, 0));
-
     while(!pq.isEmpty()) {
-      int current = pq.peek().getX();
-      int distance = -pq.peek().getY();
-      pq.poll();
+      Node curNode = pq.poll();
+      int cur = curNode.end;
 
-      if(d[current] < distance) {
+      if(check[cur]) {
         continue;
       }
-      for(int i = 0; i < vector[current].size(); i++) {
-        int next = vector[current].get(i).getX();
-        int nextDistance = distance + vector[current].get(i).getY();
+      check[cur] = true;
 
-        if(nextDistance < d[next]) {
-          d[next] = nextDistance;
-          pq.add(new Node(next, -nextDistance));
+      for(Node node : list[cur]) {
+        if(d[node.end] > d[cur] + node.weight) {
+          d[node.end] = d[cur] + node.weight;
+          pq.add(new Node(node.end, d[node.end]));
         }
       }
     }
   }
-  public static class Node {
-    int x;
-    int y;
+  public static class Node implements Comparable<Node>{
+    int end, weight;
 
-    public Node(int x, int y) {
-      this.x = x;
-      this.y = y;
+    public Node(int end, int weight){
+      this.end = end;
+      this.weight = weight;
     }
 
-    public int getX() {
-      return x;
-    }
-    public int getY() {
-      return y;
+    @Override
+    public int compareTo(Node o) {
+      return weight - o.weight;
     }
   }
 }
