9877abe5824ef9b5c8debe4f05a70c16fde36465
YongSangYou <dydtkd113@gmail.com>
2021-08-03 15:32:46 +0900

    Add 15655 solution

---
+++ src/ex15655/Main.java
@@ -0,0 +1,61 @@
+package ex15655;
+
+/*
+문제 이름: N과 M (6)
+알고리즘: 백트래킹
+자료구조: 배열
+*/
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static int n, m;
+    static int[] arr, output;
+    static boolean[] isVisited = new boolean[10001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        arr = new int[n];
+        output = new int[m];
+
+        input = br.readLine().split(" ");
+        for(int i = 0; i < n ; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(arr);
+
+        func(0);
+
+        br.close();
+        bw.close();
+    }
+
+    public static void func(int depth) {
+        if(depth == m) {
+            for(int i = 0; i < m; i++) {
+                System.out.print(output[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(!isVisited[arr[i]]) {
+                isVisited[arr[i]] = true;
+                output[depth] = arr[i];
+                if(depth > 0 && output[depth] > output[depth - 1]) {
+                    func(depth + 1);
+                } else if(depth == 0) {
+                    func(depth + 1);
+                }
+                isVisited[arr[i]] = false;
+            }
+        }
+    }
+}
