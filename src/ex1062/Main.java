66b5058f7fc47326395c02fbe1d25de900580f4b
YongSangYou <dydtkd113@gmail.com>
2021-09-19 16:46:01 +0900

    Add 1991 solution

--- src/ex1062/Main.java
+++ src/ex1062/Main.java
@@ -23,25 +23,27 @@ public class Main {
         n = Integer.parseInt(input[0]);
         int k = Integer.parseInt(input[1]);
 
+        // k가 5보다 작으면 접두사 접미사를 못배우기 때문에 0을 return
         if(k < 5) {
             System.out.println(0);
             return;
         }
 
-        char[] south = {'a', 'c', 'i', 'n', 't'};
-        int len = k - 5;
-        isLearned = new boolean[27];
+        char[] south = {'a', 'c', 'i', 'n', 't'}; // 접두사 접미사
+        int len = k - 5; // k - 접두사 접미사 알파뱃 개수
+        isLearned = new boolean[27]; // 학습한 알파벳 저장
         for(int i = 0; i < 5; i++) {
             isLearned[south[i] - 'a'] = true;
         }
 
 
-        arr = new char[n][16];
-        for(int i = 0; i < n; i++) {
+        arr = new char[n][16]; // n개의 단어에서 접두사 접미사 사이의 알파뱃을 2차원 char배열에 저장
+        for(int i = 0; i < n; i++) { // 접두사 접미사 사이의 단어 크기 이상을 돌 필요가 없으므로 나중에 조건문에 걸리도록 A로 초기화
             for(int j = 0; j < 16; j++) {
                 arr[i][j] = 'A';
             }
         }
+        // 접두사 접미사 사이의 단어를 arr배열에 저장
         for(int i = 0; i < n; i++) {
             String str = br.readLine();
             char[] charArr = str.toCharArray();
@@ -61,19 +63,24 @@ public class Main {
     }
 
     public static void backTracking(int len, int depth, int start) {
+        // 깊이가 len 일 때
         if(depth == len) {
             int cnt = 0;
+            // arr을 돌면서
             for(int i = 0; i < n; i++) {
                 boolean isCorrect = true;
                 for(int j = 0; j < arr[i].length; j++) {
+                    // A이면(= 접두사 접미사 사이의 단어가 아닐 때) break
                     if(arr[i][j] == 'A') {
                         break;
                     }
+                    // 배우지 않은 단어이면 isCorrect를 false로 하고 break
                     if(!isLearned[arr[i][j] - 'a']) {
                         isCorrect = false;
                         break;
                     }
                 }
+                // arr[i]를 다 돌고 isCorrect가 true면 cnt를 증가
                 if(isCorrect) {
                     cnt++;
                 }
