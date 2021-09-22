722c7a8cc11d3dfcc96ab092f53c3d15e1024589
YongSangYou <dydtkd113@gmail.com>
2021-09-22 14:47:26 +0900

    Add 1074 solution

--- src/ex1074/Main.java
+++ src/ex1074/Main.java
@@ -9,9 +9,7 @@ package ex1074;
 import java.io.*;
 
 public class Main {
-    static int r, c, cnt = 0, result = 0;
-    static int[][] map;
-
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,32 +17,37 @@ public class Main {
 
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
-        r = Integer.parseInt(input[1]);
-        c = Integer.parseInt(input[2]);
+        int r = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
 
         int size = (int)Math.pow(n, 2);
-        map = new int[size][size];
 
-        dfs(0, 0, (int)Math.pow(2, n));
-
+        dfs(n, r, c);
         bw.write(result + "\n");
 
         br.close();
         bw.close();
     }
 
-    public static void dfs(int x, int y, int n) {
-        if(r == y && c == x) {
-            result = cnt;
+    public static void dfs(int n, int r, int c) {
+        if(n == 0) {
             return;
         }
-        if(r < y + n && r >= y && c < x + n && c >= x) {
-            dfs(x, y, n / 2);
-            dfs(x + (n / 2), y, n / 2);
-            dfs(x, y + (n / 2), n / 2);
-            dfs(x + (n / 2), y + (n / 2), n / 2);
-        } else {
-            cnt += n * n;
+        if(r >= 0 && r <= Math.pow(2, n - 1) - 1) {
+            if(c >= Math.pow(2, n - 1) && c <= Math.pow(2, n)) { // 2사분면
+                result += Math.pow(4, n - 1);
+                dfs(n - 1, r, c - (int)Math.pow(2, n-1));
+            } else {  // 1사분면
+                dfs(n - 1, r, c);
+            }
+        } else if(r >= Math.pow(2, n - 1) && r <= Math.pow(2, n)) {
+            if(c >= Math.pow(2, n - 1) && c <= Math.pow(2, n)) { // 4사분면
+                result += Math.pow(4, n - 1) * 3;
+                dfs(n - 1, r - (int)Math.pow(2, n-1), c  - (int)Math.pow(2, n-1));
+            } else { // 3사분면
+                result += Math.pow(4, n - 1) * 2;
+                dfs(n - 1, r - (int) Math.pow(2, n - 1), c);
+            }
         }
     }
 }
