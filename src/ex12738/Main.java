0a11f7e9d8996710c6f2527c0cd22c31eb0a7bab
YongSangYou <dydtkd113@gmail.com>
2021-10-27 15:51:54 +0900

    Add 12738 solution

---
+++ src/ex12738/Main.java
@@ -0,0 +1,56 @@
+package ex12738;
+
+/*
+문제 이름: 가장 긴 증가하는 부분 수열 3
+알고리즘: 이분 탐색
+자료구조: 배열
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        String[] input = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        dp[0] = arr[0];
+        int j = 0;
+        for(int i = 1; i < n; i++) {
+            if (arr[i] > dp[j]) {
+                dp[++j] = arr[i];
+            } else {
+                int idx = lowerBound(dp, j, arr[i]);
+                dp[idx] = arr[i];
+            }
+        }
+
+        bw.write(j + 1 + "\n");
+        br.close();
+        bw.close();
+    }
+
+    public static int lowerBound(int[] arr, int right, int target) {
+        int left = 0;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if(target <= arr[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+}
