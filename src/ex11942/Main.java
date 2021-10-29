8fe25cdec1a741ccfbe01778c8710711ccba0112
YongSangYou <dydtkd113@gmail.com>
2021-10-29 23:32:06 +0900

    Add 11942 solution

---
+++ src/ex11942/Main.java
@@ -0,0 +1,20 @@
+package ex11942;
+
+/*
+문제 이름: 고려대는 사랑입니다
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("고려대학교\n");
+        br.close();
+        bw.close();
+    }
+}
