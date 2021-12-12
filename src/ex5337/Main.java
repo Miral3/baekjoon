b1163bc85dbf294c0d3383b2f61fbdec458c6b6f
YongSangYou <dydtkd113@gmail.com>
2021-12-12 16:57:42 +0900

    Add 5337 solution

---
+++ src/ex5337/Main.java
@@ -0,0 +1,20 @@
+package ex5337;
+
+/*
+문제 이름: 웰컴
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+        br.close();
+        bw.close();
+    }
+}
