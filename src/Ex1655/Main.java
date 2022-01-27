a8260d7aaa0ad27a79fdcfb2d3e9a3954a7058c5
YongSangYou <dydtkd113@gmail.com>
2022-01-27 15:59:19 +0900

    Refactor 1655 solution

--- src/Ex1655/Main.java
+++ src/Ex1655/Main.java
@@ -1,14 +1,19 @@
 package Ex1655;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/*
+문제 이름: 가운데를 말해요
+알고리즘: 우선순위 큐
+자료구조: 우선순위 큐
+*/
+
+import java.io.*;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         int testCase = Integer.parseInt(br.readLine());
@@ -29,7 +34,10 @@ public class Main {
                     maxHeap.add(minHeap.poll());
                 }
             }
-            System.out.println(maxHeap.peek());
+            bw.write(maxHeap.peek() + "\n");
         }
+
+        br.close();
+        bw.close();
     }
 }
