0348380cb0451d4fce399c173224fc7d11a731b2
YongSangYou <dydtkd113@gmail.com>
2022-01-29 22:33:11 +0900

    Add 15733 solution

---
+++ src/ex15733/Main.java
@@ -0,0 +1,20 @@
+package ex15733;
+
+/*
+문제 이름: 나는 누구인가
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("I'm Sexy\n");
+        br.close();
+        bw.close();
+    }
+}
