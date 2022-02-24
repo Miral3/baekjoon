74d5afb6c773a9554a4d212d98a86b2ff8775864
YongSangYou <dydtkd113@gmail.com>
2022-02-24 22:27:40 +0900

    Add 16170 solution

---
+++ src/ex16170/Main.java
@@ -0,0 +1,19 @@
+package ex16170;
+
+/*
+문제 이름: 오늘의 날짜는?
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("2022\n02\n24\n");
+        br.close();
+        bw.close();
+    }
+}
