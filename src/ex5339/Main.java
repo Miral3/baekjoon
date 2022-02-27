7bf1f15c51d657440f09d7917dc96e747e770518
YongSangYou <dydtkd113@gmail.com>
2022-02-27 22:39:06 +0900

    Add 5339 solution

---
+++ src/ex5339/Main.java
@@ -0,0 +1,29 @@
+package ex5339;
+
+/*
+문제 이름: 콜센터
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("     /~\\\n"
+            + "    ( oo|\n"
+            + "    _\\=/_\n"
+            + "   /  _  \\\n"
+            + "  //|/.\\|\\\\\n"
+            + " ||  \\ /  ||\n"
+            + "============\n"
+            + "|          |\n"
+            + "|          |\n"
+            + "|          |");
+        br.close();
+        bw.close();
+    }
+}
