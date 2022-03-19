35c5125cca999a2e91481590a95c7643b59609cb
YongSangYou <dydtkd113@gmail.com>
2022-03-19 17:47:44 +0900

    Add 15962 solution

---
+++ src/ex15962/Main.java
@@ -0,0 +1,20 @@
+package ex15962;
+
+/*
+문제 이름: 새로운 시작
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("파이팅!!\n");
+        br.close();
+        bw.close();
+    }
+}
