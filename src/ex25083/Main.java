562a5786f6c665fcdf773e6dd2fe515bb4ca7bde
YongSangYou <dydtkd113@gmail.com>
2022-12-14 13:48:25 +0900

    Add 25083 solution

---
+++ src/ex25083/Main.java
@@ -0,0 +1,25 @@
+package ex25083;
+
+/*
+문제 이름: 새싹
+알고리즘: 구현
+자료구조:
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |");
+        br.close();
+        bw.close();
+    }
+}
